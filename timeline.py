96988355c9aa912e27f7438b2f7fc0ee3134c94a
Keith McCammon <keith@mccammon.org>
2018-02-20 10:14:50 -0700

    Fix bug, add ability to select output data types
    - Fixed an issue that was resulting in missing process-level events.
    - Added output event types so that we can easily target specific events
      without post-processing the output file.

--- timeline.py
+++ timeline.py
@@ -6,6 +6,9 @@ import os
 import sys
 from datetime import datetime
 
+# Red Canary
+from common import *
+
 # Carbon Black
 from cbapi.response import CbEnterpriseResponseAPI
 from cbapi.response.models import Process, Sensor
@@ -18,29 +21,16 @@ else:
     _python3 = False
 
 
-def log_err(msg):
-    """Format msg as an ERROR and print to stderr.
-    """
-    msg = 'ERROR: {0}\n'.format(msg)
-    sys.stderr.write(msg)
+def process_search(cb_conn, query, query_base=None,
+                   filemods=None, netconns=None, 
+                   processes=None, regmods=None):
 
-
-def log_info(msg):
-    """Format msg and print to stdout.
-    """
-    msg = '{0}\n'.format(msg)
-    sys.stdout.write(msg)
-
-
-def convert_timestamp(datetime_obj):
-    return datetime_obj.strftime('%Y%m%d-%H%M%S')
-
-
-def process_search(cb_conn, query, query_base=None):
     if query_base != None:
         query += query_base
 
-    query_result = cb_conn.select(Process).where(query)
+    log_info("QUERY: {0}".format(query))
+
+    query_result = cb_conn.select(Process).where(query).group_by("id")
     query_result_len = len(query_result)
     log_info('Total results: {0}'.format(query_result_len))
 
@@ -50,7 +40,7 @@ def process_search(cb_conn, query, query_base=None):
         process_counter = 0
         for proc in query_result:
             process_counter += 1
-            if process_counter % 100 == 0:
+            if process_counter % 10 == 0:
                 log_info('Processing {0} of {1}'.format(process_counter, query_result_len))
 
             hostname = proc.hostname.lower()
@@ -65,21 +55,9 @@ def process_search(cb_conn, query, query_base=None):
 
             parent_name = proc.parent_name
 
-            results.append(('proc',
-                            convert_timestamp(proc.start),
-                            hostname,
-                            username,
-                            path,
-                            cmdline,
-                            process_md5,
-                            parent_name,
-                            proc.childproc_count,
-                            proc.webui_link
-                            ))
-
-            for netconn in proc.netconns:
-                results.append(('netconn',
-                                convert_timestamp(netconn.timestamp),
+            if processes == True:
+                results.append(('process',
+                                convert_timestamp(proc.start),
                                 hostname,
                                 username,
                                 path,
@@ -87,49 +65,65 @@ def process_search(cb_conn, query, query_base=None):
                                 process_md5,
                                 parent_name,
                                 proc.childproc_count,
-                                proc.webui_link,
-                                netconn.domain,
-                                netconn.remote_ip,
-                                netconn.remote_port,
-                                netconn.local_ip,
-                                netconn.local_port,
-                                netconn.proto,
-                                netconn.direction
+                                proc.webui_link
                                 ))
 
-            for filemod in proc.filemods:
-                results.append(('filemod',
-                                convert_timestamp(filemod.timestamp),
-                                hostname,
-                                username,
-                                path,
-                                cmdline,
-                                process_md5,
-                                parent_name,
-                                proc.childproc_count,
-                                proc.webui_link,
-                                '','','','','','','', # netconn
-                                filemod.path,
-                                filemod.type,
-                                filemod.md5
-                                ))
+            if netconns == True:
+                for netconn in proc.all_netconns():
+                    results.append(('netconn',
+                                    convert_timestamp(netconn.timestamp),
+                                    hostname,
+                                    username,
+                                    path,
+                                    cmdline,
+                                    process_md5,
+                                    parent_name,
+                                    proc.childproc_count,
+                                    proc.webui_link,
+                                    netconn.domain,
+                                    netconn.remote_ip,
+                                    netconn.remote_port,
+                                    netconn.local_ip,
+                                    netconn.local_port,
+                                    netconn.proto,
+                                    netconn.direction
+                                    ))
 
-            for regmod in proc.regmods:
-                results.append(('regmod',
-                                convert_timestamp(regmod.timestamp),
-                                hostname,
-                                username,
-                                path,
-                                cmdline,
-                                process_md5,
-                                parent_name,
-                                proc.childproc_count,
-                                proc.webui_link,
-                                '','','','','','','',   # netconn
-                                '','','',               # filemod
-                                regmod.path,
-                                regmod.type
-                                ))
+            if filemods == True:
+                for filemod in proc.all_filemods():
+                    results.append(('filemod',
+                                    convert_timestamp(filemod.timestamp),
+                                    hostname,
+                                    username,
+                                    path,
+                                    cmdline,
+                                    process_md5,
+                                    parent_name,
+                                    proc.childproc_count,
+                                    proc.webui_link,
+                                    '','','','','','','', # netconn
+                                    filemod.path,
+                                    filemod.type,
+                                    filemod.md5
+                                    ))
+
+            if regmods == True:
+                for regmod in proc.all_regmods():
+                    results.append(('regmod',
+                                    convert_timestamp(regmod.timestamp),
+                                    hostname,
+                                    username,
+                                    path,
+                                    cmdline,
+                                    process_md5,
+                                    parent_name,
+                                    proc.childproc_count,
+                                    proc.webui_link,
+                                    '','','','','','','',   # netconn
+                                    '','','',               # filemod
+                                    regmod.path,
+                                    regmod.type
+                                    ))
 
 
     except KeyboardInterrupt:
@@ -139,39 +133,26 @@ def process_search(cb_conn, query, query_base=None):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--profile", type=str, action="store",
-                        help="The credentials.response profile to use.")
-
-    # File output
-    parser.add_argument("--prefix", type=str, action="store",
-                        help="Output filename prefix.")
-    parser.add_argument("--append", action="store_true",
-                        help="Append to output file.")
-
-    # Time
-    parser.add_argument("--days", type=int, action="store",
-                        help="Number of days to search.")
-    parser.add_argument("--minutes", type=int, action="store",
-                        help="Number of days to search.")
-    
-    # Cb Response inputs
-    a = parser.add_mutually_exclusive_group(required=False)
-    a.add_argument("--queryfile", type=str, action="store",
-                   help="File containing queries, one per line.")
-    a.add_argument('--query', type=str, action="store",
-                   help="A single Cb query to execute.")
+    parser = build_cli_parser("Timeline utility")
 
     # Output options
-    parser.add_argument("--only-filemods", action="store_true",
-                        help="Only output file modification records.")
+    output_events = parser.add_argument_group('output_events', 
+        "If any output type is set, all other types will be suppressed unless they are explicitly set as well.")
+    output_events.add_argument("--filemods", action="store_true",
+                        help="Output file modification records.")
+    output_events.add_argument("--netconns", action="store_true",
+                        help="Output network connection records.")
+    output_events.add_argument("--processes", action="store_true",
+                        help="Output process start records.")
+    output_events.add_argument("--regmods", action="store_true",
+                        help="Output registry modification records.")
 
     args = parser.parse_args()
 
     if args.prefix:
-        output_filename = '{0}-timeline.csv'.format(args.prefix)
+        filename = '{0}-timeline.csv'.format(args.prefix)
     else:
-        output_filename = 'timeline.csv'
+        filename = 'timeline.csv'
 
     if args.append == True or args.queryfile is not None:
         file_mode = 'a'
@@ -185,13 +166,20 @@ def main():
     else:
         query_base = ''
 
-    output_all = False
-    if args.only_filemods:
-        (output_netconns, output_regmods, output_procstarts) = (False, False, False)
-        output_filemods = True
-        query_base += ' filemod_count:[1 to *]' 
+    # This is horrible. All are False by default. If all are False, then set
+    # all to True. If any are set to True, then evaluate each independently.
+    # If you're reading this and know of a cleaner way to do this, ideally via
+    # argparse foolery, by all means . . .
+    if args.filemods == False and \
+       args.netconns == False and \
+       args.processes == False and \
+       args.regmods == False:
+        (filemods, netconns, processes, regmods) = (True, True, True, True)
     else:
-        output_all = True
+        filemods = args.filemods
+        netconns = args.netconns
+        processes = args.processes
+        regmods = args.regmods
 
     if args.profile:
         cb = CbEnterpriseResponseAPI(profile=args.profile)
@@ -209,8 +197,8 @@ def main():
     else:
         queries.append('')
 
-    output_file = open(output_filename, file_mode)
-    writer = csv.writer(output_file)
+    file = open(filename, file_mode)
+    writer = csv.writer(file)
     writer.writerow(["event_type",
                      "timestamp",
                      "hostname",
@@ -236,19 +224,15 @@ def main():
                      ])
 
     for query in queries:
-        result_set = process_search(cb, query, query_base)
+        result_set = process_search(cb, query, query_base, filemods, netconns,
+                                    processes, regmods)
 
         for row in result_set:
-            if output_all == False:
-                event_type = row[0]
-                if output_filemods == True and 'filemod' not in event_type:
-                    continue
-
             if _python3 == False:
                 row = [col.encode('utf8') if isinstance(col, unicode) else col for col in row]
             writer.writerow(row)
 
-    output_file.close()
+    file.close()
 
 
 if __name__ == '__main__':
